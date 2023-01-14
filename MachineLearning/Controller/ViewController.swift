f341f70270981dcbdef1627c5746e17bdfe36406
busrademirbasozkan <busrademirbasozkan@gmail.com>
2023-01-14 20:49:32 +0300

    kullanıcı arayüzü oluşturuldu.

---
+++ MachineLearning/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Büşra Özkan on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView : UIImageView!
+    @IBOutlet weak var resultLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeButton(_ sender : Any){
+
+    }
+    
+}
+
